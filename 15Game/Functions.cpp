b0116138e46c7a347212c12bb8ef16a556c96a96
Dominiak Dominik <dominiak.dominik@ericsson.com>
2017-12-04 20:24:18 +0100

    1part

--- 15Game/Functions.cpp
+++ 15Game/Functions.cpp
@@ -9,7 +9,7 @@ void print( vector< vector<int> >  const &puzzle){
         cout << "||";
         for (int y=0; y<puzzle[x].size(); y++)
             {
-                if(puzzle[x][y] == 1 )
+                if(puzzle[x][y] == puzzle.size()*puzzle[puzzle.size()-1].size() )
                     cout <<setw(6) << "  |";
                 else
                     cout <<setw(3) << puzzle[x][y] << "  |";
@@ -88,54 +88,160 @@ vector<char> canMove(vector< vector<int> > const &puzzle,const int &xSizePuzzle,
 
         for (int z=0; z<xSizePuzzle; z++)
     {
-        if (puzzle[i][z] == 1)
+        if (puzzle[i][z] == puzzle.size()*puzzle.size())
         {
-            cout << "weszlismy";
+            if ( i>0 )
+            moves.push_back('G');
+            if ( z<xSizePuzzle && z>0 )
+            moves.push_back('L');
           if( i<ySizePuzzle -1)
                 moves.push_back('D');
-          if ( i>0 )
-            moves.push_back('G');
-          if ( z<xSizePuzzle && z>0 )
-            moves.push_back('L');
+
+
           if ( z>=0 && z<xSizePuzzle-1)
               moves.push_back('P');
         }
     }
        }
-       for(int i=0; i<moves.size(); i++)
-        cout << "mozliwe ruchy: " << moves[i] <<endl;
     return moves;
     }
 
 vector< vector<int> > doMoves(char move, vector< vector<int> > &puzzle)
 {
+
 int temp;
 for (int i=0; i<puzzle.size(); i++)
+{
+    if (temp == -1) break;
+
     for (int z=0; z<puzzle[i].size(); z++)
     {
-        if (puzzle[i][z] == 1)
+         //   cout  << "i " << i << " z " << z<<  "move " << move << endl;
+
+        if (puzzle[i][z] == puzzle.size()*puzzle.size())
         {
           if( move == 'G')
             {   temp = puzzle[i][z];
                 puzzle[i][z] = puzzle[i-1][z];
                 puzzle[i-1][z] = temp;
+                temp=-1;
+                break;
             }
-          if ( move == 'D' )
-            {   temp = puzzle[i][z];
-                puzzle[i][z] = puzzle[i+1][z];
-                puzzle[i+1][z] = temp;
-            }
+
           if ( move == 'L' )
-            {   temp = puzzle[i][z];
+
+            {
+                temp = puzzle[i][z];
                 puzzle[i][z] = puzzle[i][z-1];
                 puzzle[i][z-1] = temp;
+                temp=-1;
+                break;
             }
+
           if ( move == 'P' )
             {   temp = puzzle[i][z];
                 puzzle[i][z] = puzzle[i][z+1];
                 puzzle[i][z+1] = temp;
+                temp=-1;
+                break;
+            }
+        if ( move == 'D' )
+            {   temp = puzzle[i][z];
+                puzzle[i][z] = puzzle[i+1][z];
+                puzzle[i+1][z] = temp;
+                temp=-1;
+                break;
             }
         }
     }
+}
     return puzzle;
+
+}
+vector< vector<int> > doReverseMoves(char move, vector< vector<int> > &puzzle)
+{
+int temp;
+for (int i=0; i<puzzle.size(); i++)
+    {
+        if (temp == -1)
+            break;
+
+        for (int z=0; z<puzzle[i].size(); z++)
+        {
+            if (puzzle[i][z] == puzzle.size()*puzzle.size())
+            {
+
+            if( move == 'G')
+                {
+                    temp = puzzle[i][z];
+                    puzzle[i][z] = puzzle[i+1][z];
+                    puzzle[i+1][z] = temp;
+                    temp = -1;
+                    break;
+                }
+            if ( move == 'D' )
+                {
+                    temp = puzzle[i][z];
+                    puzzle[i][z] = puzzle[i-1][z];
+                    puzzle[i-1][z] = temp;
+                    temp = -1;
+                    break;
+                }
+            if ( move == 'L' )
+                {
+                    temp = puzzle[i][z];
+                    puzzle[i][z] = puzzle[i][z+1];
+                    puzzle[i][z+1] = temp;
+                    temp = -1;
+                    break;
+                }
+            if ( move == 'P' )
+                {
+                    temp = puzzle[i][z];
+                    puzzle[i][z] = puzzle[i][z-1];
+                    puzzle[i][z-1] = temp;
+                    temp = -1;
+                    break;
+                }
+            }
+        }
+    return puzzle;
+    }
+}
+
+bool checkCOuldBeResolve(vector< vector<int> > &puzzle){
+    int inv_count = 0;
+    int numberBlank;
+    vector<int> tempTabele;
+    for (int x=0; x<puzzle.size(); x++)
+        for (int y=0; y<puzzle[x].size(); y++)
+        {
+            if(puzzle[x][y] == puzzle.size()*puzzle.size() )
+                numberBlank = puzzle.size()-x ;
+            tempTabele.push_back(puzzle[x][y]);
+        }
+
+    for (int i = 0; i < tempTabele.size(); i++)
+    {
+        for (int j = i + 1; j <tempTabele.size(); j++)
+        {
+            // count pairs(i, j) such that i appears
+            // before j, but i > j.
+            if (tempTabele[j] && tempTabele[i] && tempTabele[i] > tempTabele[j] && tempTabele[i] != tempTabele.size())
+                inv_count++;
+        }
+    }
+    cout <<"WYNIK :  "<< numberBlank<<   "count  "<<inv_count <<  endl;
+
+
+    if (puzzle.size() & 1)
+        return !(inv_count & 1);
+
+    else     // grid is even
+    {
+        if (numberBlank & 1)
+            return !(inv_count & 1);
+        else
+            return inv_count & 1;
+    }
 }
--- 15Game/main.cpp
+++ 15Game/main.cpp
@@ -7,43 +7,96 @@
 
 using namespace std;
 
+struct element{
+int elementVvalue;
+int parentValue;
+};
+    set<U64> uniqValues;
+   vector<char> pathtable;
+
+    vector<char> dfs(vector< vector<int> > &puzzle, const int &xSizeBoard, const int &ySizeBoard)
+    {
+        cout << "++++++++++++++++++++++++++++++++++++++++++\n++++++++++++++++++++++++++++++++++++++++\n\n";
+        for (auto  z:canMove(puzzle, xSizeBoard, ySizeBoard))
+            { cout << "Zrobiono ruch: " << z  <<"\nMozliwe ruchy: ";
+                for (auto  zz:canMove(puzzle, xSizeBoard, ySizeBoard))
+                    cout << zz << " ";cout << endl;
+
+                puzzle = doMoves(z,puzzle);
+
+                U64 value = toNumber(puzzle);cout<< "VALUE " <<value << endl;
+                uniqValues.insert(value);
+                uniqValues.insert(1);
+uniqValues.insert(700);
+
+              if ( value != 0 )
+                {        print(puzzle);
+             cout << "Wartosci ";   for (auto x: uniqValues)
+                    cout << x << " ";cout << endl;
+
+                        if(*uniqValues.begin() == value)
+                            {
+                                dfs(puzzle, xSizeBoard, ySizeBoard);
+                            }
+                        else{
+                            puzzle = doReverseMoves(z, puzzle);
+                            break;
+                        }
+                }
+     //           cout << "Wstawiamy znak : " << z << endl;
+                else{
+                    pathtable.push_back(z);
+                    break;
+                }
+            }
+        return pathtable;
+    }
+
+
 int main() {
-    const int xsizePuzzle  = 4;
-    const int ySizePuzzle = 4;
+    const int xsizePuzzle  = 3;
+    const int ySizePuzzle = 3;
     vector< vector<int> > puzzle;
-    puzzle.resize(4);
+    puzzle.resize(3);
 
-    puzzle[0].push_back(16);
-    puzzle[0].push_back(7);
-    puzzle[0].push_back(15);
-    puzzle[0].push_back(9);
+    puzzle[0].push_back(1);
+    puzzle[0].push_back(2);
+    puzzle[0].push_back(3);
 
-    puzzle[1].push_back(8);
-    puzzle[1].push_back(13);
-    puzzle[1].push_back(11);
-    puzzle[1].push_back(3);
-
+    puzzle[1].push_back(9);
+    puzzle[1].push_back(4);
+    puzzle[1].push_back(5);
+    puzzle[2].push_back(7);
+    puzzle[2].push_back(8);
     puzzle[2].push_back(6);
+   /* puzzle[1].push_back(11);
+    puzzle[1].push_back(16);
+
+    puzzle[2].push_back(15);
     puzzle[2].push_back(2);
+    puzzle[2].push_back(12);
     puzzle[2].push_back(5);
-    puzzle[2].push_back(14);
 
-    puzzle[3].push_back(12);
-    puzzle[3].push_back(4);
-    puzzle[3].push_back(10);
+    puzzle[3].push_back(14);
+    puzzle[3].push_back(3);
     puzzle[3].push_back(1);
+    puzzle[3].push_back(4);
+*/
 
-    U64 num = toNumber(puzzle);
-    print(puzzle);
-    cout << "\n\nZe stanu na ukladanke :\n\n";
-    print((toPuzzle(num, xsizePuzzle,ySizePuzzle)));;
+if(checkCOuldBeResolve(puzzle))
+{
+    //poczatek
+        print(puzzle) ;
+        uniqValues.insert(toNumber(puzzle));
+        vector<char> resultTable = dfs(puzzle, xsizePuzzle, ySizePuzzle);
 
-    vector<char> xx = canMove(puzzle,xsizePuzzle,ySizePuzzle);
-    cout << "\n\n\n\n\n";
-    puzzle = doMoves('G',puzzle);
-    print(puzzle);
-    xx = canMove(puzzle,xsizePuzzle,ySizePuzzle);
+    //koniec
+    cout << "\n\n\n\n\n KONIEC \n\n\"";
+        print(puzzle);
+        for (auto x:resultTable)
+            cout << x << " " ;
+}
 
 
-    return 0;
+ return 0;
 }
